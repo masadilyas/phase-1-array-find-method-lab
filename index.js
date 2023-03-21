7d0282f499854602e9357fffe07c163b478abcfc
Massad Ilyas <+>
2023-03-21 17:54:35 -0400

    completed

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // code your solution here
-function superbowlWin(array){
-    for(let item of array){
-        if(item.result === "W"){
-            return item.year;
-        }
+// function superbowlWin(array){
+//     for(let item of array){
+//         if(item.result === "W"){
+//             return item.year;
+//         }
+//     }
+// }
+
+function superbowlWin(record){
+    const win = record.find(game => game.result === 'W')
+    if (win) {
+        return win.year
+    }
+    else {
+        return undefined
     }
 }
-
